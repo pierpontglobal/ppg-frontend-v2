12498f314cc86c44f9b10a80680709582256830e
Hector Acosta <hector.acosta.pozo@outlook.com>
2020-01-31 12:34:23 -0400

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { simpleAction } from './redux/actions/simpleAction'
 
-function App() {
+function App(props) {
+
+  const simpleAction = (event) => {
+    props.simpleAction();
+   }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={simpleAction}>Test redux action</button>
       </header>
+
+      <pre>
+        {
+          JSON.stringify(props)
+        }
+      </pre>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
